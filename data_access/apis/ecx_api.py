0df8cf7d5e0fa0b445f6538b38abfa049cd37503
PhamCongHiep2003 <84440518+PhamCongHiep2003@users.noreply.github.com>
2024-05-27 19:40:06 +1000

    Initial commit

--- data_access/apis/ecx_api.py
+++ data_access/apis/ecx_api.py
@@ -1,11 +1,37 @@
-from application.src.abstractions.apis.ecx_api_interface import EcxApiInterface
-
+import requests
 
 class EcxApi(EcxApiInterface):
-    def fetch_critical_parameters(self):
-        # code to fetch data from the critical parameters endpoint
-        pass
+    API_KEY = 'JjsFazxTPd7GVoPYGdEI34HrudDZHq695FqKKnmU'
+    BASE_URL = 'https://api.edgeapi-v1.com/swinburn'
+
+    def fetch_critical_parameters(self, device_serialid, starttime, endtime):
+        """
+        Fetch critical parameters from the EdgeConX API.
+        
+        :param device_serialid: Serial ID of the device
+        :param starttime: Start time of the data interval (Unix timestamp)
+        :param endtime: End time of the data interval (Unix timestamp)
+        :return: JSON response containing critical parameters
+        """
+        url = f"{self.BASE_URL}/powerquality/interval/{device_serialid}?starttime={starttime}&endtime={endtime}"
+        headers = {'x-api-key': self.API_KEY}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
 
     def fetch_device_parameters(self):
-        # code to fetch data from the critical parameters endpoint
-        pass
+        """
+        Fetch device parameters from the EdgeConX API.
+        
+        :return: JSON response containing device parameters
+        """
+        url = f"{self.BASE_URL}/devices"
+        headers = {'x-api-key': self.API_KEY}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+# Example usage:
+# ecx_api = EcxApi()
+# critical_params = ecx_api.fetch_critical_parameters('EE40400611940036', 1713621600, 1713708000)
+# device_params = ecx_api.fetch_device_parameters()
