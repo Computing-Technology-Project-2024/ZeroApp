6c710723064a4d2c01c829f5ca0de68f2dce7060
Quang (Austin) Dinh <thangdhm75@gmail.com>
2024-10-23 17:34:05 +1100

    update setting and sidebar

--- web_app/src/components/sideBars/SideBar.jsx
+++ web_app/src/components/sideBars/SideBar.jsx
@@ -5,44 +5,37 @@ import logo from '../../img/logo.png';
 import dash_icon from '../../img/dash_icon.png';
 import analytics_ico from '../../img/analytics_icon.png';
 import recon_icon from '../../img/recommend_icon.png';
-import expen_icon from '../../img/expenses_icon.png';
+import logout_icon from '../../img/logout_icon.png';
+import settings_icon from '../../img/settings_icon.png';
 import {Link} from "react-router-dom";
 import {pageRoutes} from "../../constants/pageRoutes";
 
-const Sidebar = () => {
+const Sidebar = ({ className }) => {
   const [activeItem, setActiveItem] = useState('dashboard');
 
   const handleItemClick = (item) => {
     setActiveItem(item);
   };
 
-  const pageItems = [
-    { name: 'pricing', text: 'temp', route: pageRoutes.HOME },
-    { name: 'calendar', text: 'temp', route: pageRoutes.HOME },
-    { name: 'to-do', text: 'temp', route: pageRoutes.HOME },
-    { name: 'contact', text: 'temp', route: pageRoutes.HOME },
-    { name: 'invoice', text: 'temp', route: pageRoutes.HOME },
-  ];
-
   const settingsItems = [
-    { name: 'settings', text: 'Settings', route: pageRoutes.SETTINGS },
-    { name: 'logout', text: 'temp', route: pageRoutes.LOGIN },
+    { name: 'settings', icon: settings_icon, text: 'Settings', route: pageRoutes.SETTINGS },
+    { name: 'logout', icon: logout_icon, text: 'Logout', route: pageRoutes.LOGIN },
   ];
 
   const menuItems = [
     { name: 'dashboard', icon: dash_icon, text: 'Dashboard', route: pageRoutes.DASHBOARD },
     { name: 'analytics', icon: analytics_ico, text: 'Analytics', route: pageRoutes.ANALYTICS },
-    { name: 'recommendation', icon: recon_icon, text: 'Recommendation', route: pageRoutes.RECOMMENDATIONS },
-    { name: 'expense', icon: expen_icon, text: 'temp', route: pageRoutes.HOME }
+    { name: 'recommendation', icon: recon_icon, text: 'Recommendation', route: pageRoutes.RECOMMENDATIONS }
   ];
 
   return (
-      <div className="sidebar">
+      <div className={`sidebar w-60 ${className}`}>
         <div className="logo">
           {/* <span className="logo-zero">Zero</span>
           <span className="logo-app">App</span> */}
           <img src={logo} alt="Logo"/>
         </div>
+
         <nav className="menu">
           <ul>
             {menuItems.map((item) => (
@@ -51,36 +44,15 @@ const Sidebar = () => {
                     className={`menu-item ${activeItem === item.name ? 'active' : ''}`}
                 >
                   <Link to={item.route} onClick={() => handleItemClick(item.name)}>
-                    <img className="img" src={item.icon} alt="icon"/> {item.text}
+                    <img className="img" src={item.icon} alt="icon"/>
+                    <p>{item.text}</p>
                   </Link>
                 </li>
             ))}
           </ul>
         </nav>
 
-        <div className="hr">
-          <hr/>
-        </div>
-
-        <div className="pages">
-          Pages
-          <ul>
-            {pageItems.map((item) => (
-              <li
-                key={item.name}
-                className={`menu-item ${activeItem === item.name ? 'active' : ''}`}
-              >
-                <Link to={item.route} onClick={() => handleItemClick(item.name)}>
-                  {item.text}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="hr">
-          <hr/>
-        </div>
+        <hr></hr>
 
         <div className="settings">
           <ul>
@@ -90,7 +62,7 @@ const Sidebar = () => {
                 className={`menu-item ${activeItem === item.name ? 'active' : ''}`}
               >
                 <Link to={item.route} onClick={() => handleItemClick(item.name)}>
-                  {item.text}
+                <img className="img" src={item.icon} alt="icon"/> {item.text}
                 </Link>
               </li>
             ))}
